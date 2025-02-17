c9f382ca09c653357011f0b9fd504c13af5c30d1
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-02-16 22:53:18 -0500

    Foundational stuff.

--- src/SavedVariable.tsx
+++ src/SavedVariable.tsx
@@ -1,5 +1,10 @@
 class SavedVariable<T> {
-    constructor(public value: T) {}
+    private value: T;
+
+    constructor(value: T) {
+        this.value = value;
+    }
+
     getValue(): T {
         return this.value;
     }
