a75c3ddb6ef54c72ab45ea13b8da950488ac27db
Lord-Raven <93345679+Lord-Raven@users.noreply.github.com>
2025-02-19 22:34:07 -0500

    Formatting start menu.

--- src/display/TitleScreen.tsx
+++ src/display/TitleScreen.tsx
@@ -17,7 +17,7 @@ export const TitleScreen: FC<TitleScreenProps> = ({ stage, setOnMenu }) => {
     const [progress, setProgress] = useState<number>(0);
     const [progressLabel, setProgressLabel] = useState<string>('');
     const {displayError} = useError();
-    const buttonProps = {sx: {outline: 1, bgcolor: '#00000088', color: 'primary', variant: 'h3', m: 1}};
+    const buttonProps = {sx: {outline: 1, backgroundColor: '#000000CC', color: 'primary', typography: 'h3', m: 1}};
 
     const updateProgress = (progress: number, label: string) => {setProgress(progress);setProgressLabel(label)};
     const handleGenerateClick = () => {
@@ -42,9 +42,10 @@ export const TitleScreen: FC<TitleScreenProps> = ({ stage, setOnMenu }) => {
             backgroundRepeat: 'no-repeat',
             width: '100vw',
             height: '100vh',
-            color: '#ffffff'
+            color: '#ffffff',
+            verticalAlign: 'middle'
         }}>
-            <div style={{display: 'flex', flexDirection: 'column', gap: '2vh', height: '100vh', width: '100vw', alignItems: 'center', verticalAlign: 'middle'}}>
+            <div style={{display: 'flex', flexDirection: 'column', gap: '2vh', width: '100vw', alignItems: 'center'}}>
                 {generating ? (
                     <>
                         <Box sx={{backgroundColor: '#00000088', width: '80%'}} color={'primary'}>
